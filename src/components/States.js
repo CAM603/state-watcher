99dbd97a9f4c156e7760bca268f89c4b673e68bd
CAM603 <cameronhawley@comcast.net>
2020-04-03 19:01:56 -0400

    ADD map

--- src/components/States.js
+++ src/components/States.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import {getStates} from '../actions/statesAction'
 import Wrapper from './Wrapper';
+import {VectorMap} from 'react-jvectormap';
 
 const States = (props) => {
     useEffect(() => {
         props.getStates()
     }, [])
 
-
+    const handleRegionClick = (e, data) => {
+        setTimeout(()=> { Array.from(document.getElementsByClassName("jvectormap-tip")).forEach((el) => { el.style.display = 'none' }); },100)
+    }
     return (
         <Wrapper {...props}>
-            {props.loading ?
+            {/* {props.loading ?
             <div class="ui vertically divided grid">
                 <div class="one column row">
                     <div class="column">
@@ -31,7 +34,20 @@ const States = (props) => {
                     <p>{state.name}</p>
                 </Link>
             ))
-            }
+            } */}
+            <div style={{width: 500, height: 500}}>
+            <VectorMap 
+                map={'us_aea'}
+                backgroundColor="#3b96ce"
+                useRef='map'
+                containerStyle={{
+                    width: '100%',
+                    height: '100%'
+                }}
+                containerClassName="map"
+                onRegionClick={handleRegionClick}
+            />
+            </div>
         </Wrapper>
     )
 }
