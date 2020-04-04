6cde5d5a746517fc34f97b4fb262cf267407d534
CAM603 <cameronhawley@comcast.net>
2020-04-04 11:34:52 -0400

    ADD posts list

--- src/components/State.js
+++ src/components/State.js
@@ -14,20 +14,49 @@ const State = (props) => {
 
   const renderScreen = () => {
     return (
-      <div>
-        <h1>Welcome to {props.currentState.name}</h1>
-        <div>
-          <StateIssues issues={props.issues}/>
+      <>
+        <div className="ui top attached menu">
+          <div className="ui simple dropdown icon item">
+            <i className="add icon"></i>
+            <div className="menu">
+              <div className="item">
+                <i className="dropdown icon"></i>
+                <span className="text">New</span>
+                <div className="menu">
+                  <div className="item">Post</div>
+                  <div className="item">Image</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h2 style={{padding: '10px', marginLeft: '15px'}}>Welcome to {props.currentState.name}</h2>
+          </div>
+        
+          <div className="right menu">
+            <div className="ui right aligned category search item">
+              <div className="ui transparent icon input">
+                <input className="prompt" type="text" placeholder="Search posts..."/>
+                <i className="search link icon"></i>
+              </div>
+              <div className="results"></div>
+            </div>
+          </div>
         </div>
-      </div>
+        <div className="ui bottom attached segment">
+          <div>
+            <StateIssues issues={props.issues}/>
+          </div>
+        </div>
+      </>
     )
   }
   const renderLoading = () => {
-    return <div class="ui active centered loader"></div>
+    return <div className="ui active centered loader"></div>
   }
   return (
     <Wrapper {...props}>
-      {props.loadingState ? renderLoading() : renderScreen()}
+        {props.loadingState ? renderLoading() : renderScreen()}
     </Wrapper>
   )
 }
