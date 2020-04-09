6a1af72056e78772ff80b217b17044d1d805fd6e
CAM603 <cameronhawley@comcast.net>
2020-04-09 10:46:01 -0400

    ADD onboarding scss

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,68 +6,66 @@ import logo from '../images/logo192.png';
 import { login } from '../actions/loginAction';
 
 const Login = (props) => {
-    const [user, setUser] = useState({
-        username: '',
-        password: ''
+  const [user, setUser] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChanges = (event) => {
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value
     })
+}
 
-    const handleChanges = (event) => {
-        setUser({
-            ...user,
-            [event.target.name]: event.target.value
-        })
-    }
-
-    const handleSubmit = event => {
-        event.preventDefault()
-        props.login(user, props)
-        setUser({username: '', password: ''})
-
-    }
-    const renderLoading = () => {
-        return (
-            <div class="ui active dimmer">
-                <div class="ui loader"></div>
-            </div>
-        )
-    }
-    const renderLogin = () => {
-        return (
-            <div>
-                
-                    <form onSubmit={handleSubmit}>
-                        
-                                    <input
-                                        type="text"
-                                        name="username"
-                                        placeholder="username"
-                                        onChange={handleChanges}
-                                        value={user.username}
-                                    />
-                            
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        placeholder="password"
-                                        onChange={handleChanges}
-                                        value={user.password}
-                                    />
-                                
-                            <button>Log In</button>
-                        
-                    </form>
-                    <div>
-                        New to us? <Link to="/register">Sign Up</Link>
-                    </div>
-                
-            </div>
-        )
-    }
+  const handleSubmit = event => {
+      event.preventDefault()
+      props.login(user, props)
+      setUser({username: '', password: ''})
+  }
+  const renderLoading = () => {
+      return (
+          <div>
+              <div></div>
+          </div>
+      )
+  }
+  const renderLogin = () => {
     return (
-        <>
-            {props.loading ? renderLoading() : renderLogin()}
-        </>
+      <>
+        <div className="onboarding-container"></div>
+          <div className="login-container">
+            <div className="login">
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="username"
+                  onChange={handleChanges}
+                  value={user.username}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  onChange={handleChanges}
+                  value={user.password}
+                />
+                <button>Log In</button>
+              </form>
+              <div className="login-footer">
+                <span>New to us? <Link to="/register">Sign Up</Link></span>
+              </div>
+            </div>
+          </div>
+      </>
     )
+  }
+  return (
+    <>
+      {props.loading ? renderLoading() : renderLogin()}
+    </>
+  )
 }
 const mapStateToProps = state => {
     return {
--- src/components/Register.js
+++ src/components/Register.js
@@ -6,53 +6,54 @@ import logo from '../images/logo192.png'
 import { register } from '../actions/registerAction'
 
 const Register = ({register}) => {
-    const [user, setUser] = useState({
-        username: '',
-        password: ''
+  const [user, setUser] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChanges = event => {
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value 
     })
+  }
 
-    const handleChanges = event => {
-        setUser({
-            ...user,
-            [event.target.name]: event.target.value 
-        })
-    }
+  const handleSubmit = event => {
+    event.preventDefault()
 
-    const handleSubmit = event => {
-        event.preventDefault()
-
-        register(user)
-        setUser({username: '', password: ''})
-    }
+    register(user)
+    setUser({username: '', password: ''})
+  }
     
-    return (
-        <div>
-                <form onSubmit={handleSubmit}>
-                
-                                <input
-                                    type="text"
-                                    name="username"
-                                    placeholder="username"
-                                    onChange={handleChanges}
-                                    value={user.username}
-                                />
-                            
-                                <input
-                                    type="password"
-                                    name="password"
-                                    placeholder="password"
-                                    onChange={handleChanges}
-                                    value={user.password}
-                                />
-                            
-                        <button>Sign up</button>
-                </form>
-                <div>
-                    Already a member? <Link to="/login">Login</Link>
-                </div>
+  return (
+    <>
+      <div className="onboarding-container"></div>
+        <div className="login-container">
+          <div className="login">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="username"
+                placeholder="username"
+                onChange={handleChanges}
+                value={user.username}
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                onChange={handleChanges}
+                value={user.password}
+              />
+              <button>Sign up</button>
+            </form>
+            <div className="login-footer">
+              <span>Already a member? <Link to="/login">Login</Link></span>
+            </div>
+          </div>
         </div>
-        
-    )
+    </>
+  )
 }
 
 export default connect(null, {register})(Register);
