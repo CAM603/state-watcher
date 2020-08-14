96c2c7856322deca99b397f0e18a3e93c8fe06e6
CAM603 <cameronhawley@comcast.net>
2020-08-14 08:24:33 -0400

    add readme info and adjust landing page style

--- src/components/Onboarding.js
+++ src/components/Onboarding.js
@@ -1,43 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Onboarding = (props) => {
-  const loggedIn = localStorage.getItem('token')
-  const message = localStorage.getItem('message')
+    const loggedIn = localStorage.getItem("token");
+    const message = localStorage.getItem("message");
 
-  return (
-    <>
-    <div className="onboarding-container">
-    </div>
-      <header>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Sign Up</Link>
-      </header>
-      <main>
-        <section className="card">
-          <ul>
-            <li>
-              <span></span>
-              <strong>Do you travel bro?</strong>
-            </li>
-            <li>
-              <span></span>
-              <strong>Do you visit multiple states?</strong>
-            </li>
-            <li>
-              <span></span>
-              <strong>State Watcher can help</strong>
-            </li>
-          </ul>
-        </section>
-        <section className="primary">
-          <h1>State Watcher</h1>
-          <p>See something, say something</p>
-          <Link to="/register">Get started</Link>
-        </section>
-      </main>
-      </>
-  )
-}
+    return (
+        <>
+            <div className="onboarding-container"></div>
+            <header>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Sign Up</Link>
+            </header>
+            <main>
+                <section className="card">
+                    <ul>
+                        <li>
+                            <span></span>
+                            <strong>Do you travel often?</strong>
+                        </li>
+                        <li>
+                            <span></span>
+                            <strong>Do you visit multiple states?</strong>
+                        </li>
+                        <li>
+                            <span></span>
+                            <strong>Have issues to report?</strong>
+                        </li>
+                        <li>
+                            <span></span>
+                            <strong>
+                                Wish you knew about that road closure?
+                            </strong>
+                        </li>
+                        <li>
+                            <span></span>
+                            <strong>State Watcher can help</strong>
+                        </li>
+                    </ul>
+                </section>
+                <section className="primary">
+                    <h1>State Watcher</h1>
+                    <p>See something, say something</p>
+                    <Link to="/register">Get started</Link>
+                </section>
+            </main>
+        </>
+    );
+};
 
-export default Onboarding
+export default Onboarding;
